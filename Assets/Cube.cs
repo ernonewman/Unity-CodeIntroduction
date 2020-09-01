e43d890bf28937a7834d59896b006199de25f430
Erno Newman <ernonewman@gmail.com>
2020-09-01 19:01:14 +0100

    Section 2: Introduction to Coding - Part 10 - Variables

---
+++ Assets/Cube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float SizeModifier = 2.5f;
+
+    public string newName = "Cubey1337";
+
+    public bool isRotated = false;
+}
