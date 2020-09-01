a744d33ccc4495539a41515adbaf402350317c33
Erno Newman <ernonewman@gmail.com>
2020-09-01 19:29:13 +0100

    Part 13 - Loops

---
+++ Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int cubeAmount = 10;
+    public float randomArea = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cubeAmount; i++)
+        {
+            var cubeObject = Instantiate(cubePrefab);
+            cubeObject.transform.position = new Vector3(
+                Random.Range(-randomArea, randomArea),
+                Random.Range(-randomArea, randomArea),
+                0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
