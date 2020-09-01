6f113cf53d6fdbcd80b909092ad144cba6c954f0
Erno Newman <ernonewman@gmail.com>
2020-09-01 19:12:13 +0100

    Part 11 - Methods

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -9,4 +9,22 @@ public class Cube : MonoBehaviour
     public string newName = "Cubey1337";
 
     public bool isRotated = false;
+
+    // Used for initialization
+    void Start()
+    {
+        transform.localScale *= SizeModifier;
+        transform.name = newName;
+    }
+
+    // Called once per frame
+    private void Update()
+    {
+        transform.localScale = Vector3.one * SizeModifier;
+    }
+
+    string ImproveName(string originaleString)
+    {
+        return $"-[{originaleString}]-";
+    }
 }
